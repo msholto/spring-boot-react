9693fe0a8e38d8c8f8f1d303a3f2b2fbe0c1ab0a
Matej Soltysik <msholto@gmail.com>
2019-11-16 20:44:00 +0100

    disable csrf for dev profile

--- src/main/java/sk/msholto/app/configurations/DevSecurityConfiguration.java
+++ src/main/java/sk/msholto/app/configurations/DevSecurityConfiguration.java
@@ -29,7 +29,12 @@ public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
     }
 
     @Bean
